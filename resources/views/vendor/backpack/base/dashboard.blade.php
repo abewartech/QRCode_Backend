1661ab88c5b84f628ccb72c9e5e63314dd805df5
ABE <yusufarifin89@gmail.com>
2022-02-06 11:01:26 +0700

    AB : Fix

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -3,13 +3,13 @@
 <div class="row">
     @foreach(\App\Models\QRCode::all() as $item)
     <div class="col">
-        <div class="card" style="width: 18rem;">
-            <div class="card-img-top mx-auto">
+        <div class="card" style="border-radius: 18px;">
+            <div class="d-flex justify-content-center pt-3">
                 {!!$item->qrcode!!}
             </div>
-            <div class="card-body">
-                <h5 class="card-title">{{$item->name}}</h5>
-                <p class="card-text">{{\App\Models\ScanHistory::where('qr_codes_id', $item->id)->count()}}</p>
+            <div class="card-body d-flex flex-column align-items-center">
+                <h4 class="card-title">{{$item->name}}</h4>
+                <p class="card-text font-weight-bolder">{{\App\Models\ScanHistory::where('qr_codes_id', $item->id)->count()}}</p>
             </div>
         </div>
     </div>
